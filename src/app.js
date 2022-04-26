2452f0935cb81f13d9d09d5cb597406579d10225
Johnny Chen <96746703+JohnnyChen129@users.noreply.github.com>
2022-04-26 11:59:48 -0400

    GrubDash

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-
-if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const app = express();
-const moviesRouter = require("./movies/movies.router");
-const reviewsRouter = require("./reviews/reviews.router");
-const theatersRouter = require("./theaters/theaters.router");
-const cors = require('cors');
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/movies", moviesRouter);
-app.use("/reviews", reviewsRouter);
-app.use("/theaters", theatersRouter);
+const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+const ordersRouter = require("./orders/orders.router");
+const dishesRouter = require("./dishes/dishes.router");
+
+const app = express();
+
+// You have not learned about CORS yet.
+// The following line let's this API be used by any website.
+app.use(cors());
+app.use(express.json());
+
+app.use("/dishes", dishesRouter);
+app.use("/orders", ordersRouter);
 
 app.use(notFound);
+
 app.use(errorHandler);
 
 module.exports = app;
